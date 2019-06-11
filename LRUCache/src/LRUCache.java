e3dfd3384175e33892c0ce3ef78fc8c7d5282222
Shubham Priyadarshi <shubham.priyadarshi@moglix.com>
2019-06-12 03:09:05 +0530

    linked list changes

--- LRUCache/src/LRUCache.java
+++ LRUCache/src/LRUCache.java
@@ -4,6 +4,7 @@
 */
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,26 +12,23 @@ import java.util.Map;
 public class LRUCache<K, V> implements Serializable {
 	private static final long serialVersionUID = 4L;
 	private int sizeLimit; // Size limit of the LRUCache(Maximum size)
-//	private List<K> list = new ArrayList();
-	private DoublyLL<K, V> list; // Doubly linked list to maintain LRU and MRU
-	private Map<K, Node<K, V>> cache; // Maps key vs node location in Doubly linked list
+	private List<K> list;
+	private Map<K, V> cache; // Maps key vs node location in Doubly linked list
 	
 	public LRUCache(int sizeLimit) {
 		this.sizeLimit = sizeLimit;
 		cache = new HashMap<>();
-		list = new DoublyLL<K, V> ();
+		list = new ArrayList<>();
 	}
 	
 	/* Returns the value associated with the key in the LRU cache */
 	public synchronized V get(K key) {
 		if(cache.containsKey(key)) { // Key present in cache
-			Node<K, V> node = cache.get(key);
-			V retVal = node.getVal();
-			list.delete(node); // Delete from list
+			V retVal = cache.get(key);
+			list.remove(key); // Delete from list
 			cache.remove(key); // Delete from cache
-			node = new Node<K, V>(key, retVal);
-			list.insert(node); // Insert into list as the Most recently used
-			cache.put(key, node); // Update the cache
+			list.add(key); // Insert into list as the Most recently used
+			cache.put(key, retVal); // Update the cache
 			return retVal;
 		}
 		return null; // Key not present in the cache
@@ -39,32 +37,30 @@ public class LRUCache<K, V> implements Serializable {
 	/* Inserts the key, value as most recently used element in the LRU cache */
 	public synchronized void put(K key, V val) {
 		if(cache.containsKey(key)) { // Key already present in cache
-			Node<K, V> node = cache.get(key);
-			list.delete(node); // Delete old node from list
+			list.remove(key); // Delete old node from list
 			cache.remove(key); // Update the cache
-			Node<K, V> newNode = new Node<K, V> (key, val);
-			list.insert(newNode); // Insert new node into list
-			cache.put(key, newNode);			
+			list.add(key); // Insert new node into list
+			cache.put(key, val);
 		}
 		else { // New key to be inserted
 			if(cache.size() == sizeLimit) { // Cache is full
-				Node<K, V> leastRecent = list.getLRU(); // Get least recently used node
-				K leastRecentKey = leastRecent.getKey();
+				K leastRecentKey = list.get(0); // Get least recently used node
 				cache.remove(leastRecentKey); // Delete least recently used from cache
-				list.delete(leastRecent); // Update list
-				Node<K, V> newNode = new Node<K, V>(key, val);
-				cache.put(key, newNode); // Insert new node into cache
-				list.insert(newNode);
+				list.remove(leastRecentKey); // Update list
+				cache.put(key, val); // Insert new node into cache
+				list.add(key);
 			}
 			else { // Cache has space
-				Node<K, V> newNode = new Node<K, V>(key, val);
-				cache.put(key, newNode); // Insert into cache
-				list.insert(newNode); // Insert into list
+				cache.put(key, val); // Insert into cache
+				list.add(key); // Insert into list
 			}
 		}
 	}
 
-	public void print() {
-		list.print();
+	public synchronized void print() {
+		for (int i = 0; i < list.size(); i++) {
+			System.out.print(cache.get(list.get(i)) + " ");
+		}
+		System.out.println();
 	}
 }
